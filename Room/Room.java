3c23aef55616e705ef3b543892c0c92b61938150
ali <gurchina.alina13@gmail.com>
2023-06-28 19:01:12 +0200

    Room

--- Room/Room.java
+++ Room/Room.java
@@ -3,10 +3,10 @@ package Room;
 import java.util.Objects;
 
 public class Room {
-    private RoomType type;
+    private  RoomType type;
     private final double area;
     private final int windowCount;
-    private int balconyCount;
+    private  final int balconyCount;
 
     public Room(RoomType type, double area, int windowCount, int balconyCount) {
         this.area = area;
@@ -18,9 +18,7 @@ public class Room {
         return type;
     }
 
-    public void setType(RoomType type) {
-        this.type = type;
-    }
+
 
     public double getArea() {
         return area;
@@ -30,9 +28,7 @@ public class Room {
         return balconyCount;
     }
 
-    public void setBalconyCount(int balconyCount) {
-        this.balconyCount = balconyCount;
-    }
+
 
     public int getWindowCount() {
         return windowCount;
